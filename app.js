08aaae95b611b27c7bb52fa57c611d99fe422faf
HamadTariq120 <hamadtariqly@gmail.com>
2020-06-14 14:17:38 +0500

    java Assignment 5

---
+++ app.js
@@ -0,0 +1,273 @@
+// alert("Chap #1 Start")
+// alert("Wellcom to my webpage")
+// alert("Error! Please enter a valid password")
+// alert("wellcome to js land \n Happy coding")
+
+// alert("wellcom to js land")
+// alert("Happy cooding \n pervent this page for creating dialoge" )
+// alert("Hello! i can run js through my browser console")
+// alert("Chapter #2 Start")
+// var a = username
+// var my_name="Muhammad Hamad Tariq"
+// var titled_message ="Hello world";
+// alert(titled_message)
+
+// var student_name= "john deo";
+//  var age= "15 year old";
+//  var work= "Certified Mobile developer";
+//  alert(student_name)
+// alert(age);
+// alert(work);
+
+// var a ="Pizza \n Pizz \n Piz \n Pi \n P";
+// alert(a)
+
+// var Email= "hamadtariqly@gmail.com";
+// alert("My Email address is "+ Email)
+// var book=" A smarter way to learn JavaScript"
+// alert("im trying to learn" + book )
+// document.write("im using java scipt in my browser")
+// Chapter #3
+// var age=22;
+// alert("I am 22 year old")
+
+// var name=Hammad;
+// var title="5 Tshirt(S)";
+// var Quantity=XYZ
+// document.write(name + "orderd" + title+ "on" + Quantity + "Clothing store");
+
+// Chap #4
+
+// var a,b,c =( "Hammad","Tariq" ,"ALi")
+// alert(a)
+// document.write("<h1> Rules for naming js variables </h1><br> Variable names can only contain For example. $name, _name or name <br>Variable names are case senstive <br> Variable names should not be JS keywords ")
+
+// Chap #5
+// var a=3;
+// var b=5;
+// var c =a+b;
+// document.write("sum of" + a +"and" +b +"is equal to " +c)
+
+// var a=3;
+// var b=5;
+// var c =a*b;
+// document.write("multiple of" + a +"and" +b +"is equal to " +c)
+// var a=3;
+// var b=5;
+// var c =a/b;
+// document.write("devision of" + a +"and" +b +"is equal to " +c)
+
+
+// var initial_value=5
+// var inc= ++initial_value
+// var add= initial_value+ inc
+// var dec=--add
+// var reminder=++add%3
+// document.write(" Value after variable declaration is:<br> "+"initial value is"+ initial_value +"<br>" +"after increment the value is "+inc +"<br>"+ "value after addition is"+add+"<br>" +"The value after decremnt is "+ dec +"<br>"+"The reminder is"+ reminder )
+
+// var ticket=600;
+// var total=5;
+// document.write("Total cost of movie" +total+ "ticket "+ "is"+total*ticket)
+// var t=3;
+// document.write("3"+"X"+"1"+"="+t*1 +"<br>"+"3"+"X"+"2"+"="+t*2 +"<br>"+"3"+"X"+"3"+"="+t*3 +"<br>"+"3"+"X"+"4"+"="+t*4 +"<br>"+"3"+"X"+"5"+"="+t*5 +"<br>"+"3"+"X"+"6"+"="+t*6 +"<br>"+"3"+"X"+"7"+"="+t*7 +"<br>"+"3"+"X"+"8"+"="+t*8 +"<br>"+"3"+"X"+"9"+"="+t*9 +"<br>"+"3"+"X"+"10"+"="+t*10 +"<br>")
+
+// var c=(25-32)*5/9;
+// var F=(75*9/5)+32
+// document.write("25'f is"+c+"C'"+"<br>"+"75 'C is"+F+"F'")
+
+// var item1=650;
+// var Quantity1=3;
+// var total1=item1 * Quantity1;
+// var item2=100;
+// var Quantity2=7;
+// var total2=item2 * Quantity2;
+// var charges=100;
+// document.write("<h1> Shopping Chart</h1>"+"<br>"+"Total cost of your order is"+" " + total1+ total2 + charges)
+
+// var student=685;
+// var total=1100
+// document.write("your result is"+" "+ student*100/total +"%")
+
+// var total=980;
+// var obt=802
+// document.write("Total marks is"+" "+total+"<br>"+"Obtain markes is" +" "+obt+"<br>"+ "Percentage is"+" " +obt*100/total);
+
+// Chapter #6 to 9
+// var a =10;
+// document.write("The value of a is"+" "+ a+"<br>"+"'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"+"<br>"+"The value of ++a is "+" "+ ++a+"<br>"+"<br>"+"Now the value of a is"+" "+ ++a+"<br>"+"<br>"+"The value of --a is"+" "+ --a +"<br>"+"Now the value of a is 11"+"<br>"+"The value of a-- is "+" "+a--+"<The value of a-- is>"+" "+--a)
+
+// var a=2,b=1;
+// document.write("The value of a is"+" "+a+"<br>"+ "The value of b is"+" "+b +"<br>"+ "Total value is"+" "+ [--a- --b+ ++b + b--])
+
+// var name=prompt("Enter your name is","Your name is")
+
+// var a=prompt("Enter your value","Your value is");
+// var b=5;
+// alert("Your answer is"+ " "+a*5 )
+// var total=100
+// var eng=54,math=54,urdu=48;
+// document.write("<table>");
+// document.write("<h1>" +"Total marks obtain & total percentage is")
+// document.write("<tr>"+"<td>"+"English"+ " "+ total+" "+ eng+" "+eng*100/total+"%"+"</td>"+"</tr>")
+// document.write("<tr>"+"<td>"+"Urdu"+ " "+ total+" "+ urdu+" "+urdu*100/total+"%"+"</td>"+"</tr>")
+// document.write("<tr>"+"<td>"+"Math"+ " "+ total+" "+ math+" "+math*100/total+"%"+"</td>"+"</tr>")
+
+
+// Chapter #9 to 11
+
+// var city=prompt("Enter city name");
+// if("city===Karachi"){
+//     alert("''Wellcome to city of lights'")
+// }
+// var gender =prompt("Please enter your gender");
+// if("gender===Male"){
+//     alert("Good Morning Sir!")
+// }
+// else if("gender===Female"){
+//     alert("Good Morning Mam!")
+// }
+// else{
+//     alert("Wrong Entry")
+// // }
+// document.write("table");
+// document.write("<th>" +"<td>"+"<H3>" +"Signal Color"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+"Message");
+// document.write("<th>" +"<td>"+"<H3>" +"RED"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+"Must stop");
+// document.write("<th>" +"<td>"+"<H3>" +"YELLOW"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+"Ready to move")
+// document.write("<th>" +"<td>"+"<H3>" +"GREEN"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "+"Move now")
+
+
+// var fuel=0.2;
+// if("fuel<0.25"){
+//     alert("Please refil the fuel of your car")
+// }
+
+// var a = 4; if (++a === 5)
+// { alert("given condition for variable a is true"); } 
+
+//  var a = 4; if (++a === 5){ alert("given condition for variable a is true"); } 
+ 
+// var b = 82; if (b++ === 83){ alert("given condition for variable b is true"); } 
+ 
+//  var c = 12; if (c++ === 13){ alert("condition 1 is true"); } if (c === 13){ alert("condition 2 is true"); } if (++c < 14){ alert("condition 3 is true"); } if(c === 14){ alert("condition 4 is true"); } 
+ 
+//  var materialCost = 20000; var laborCost = 2000; var totalCost = materialCost + laborCost; if (totalCost === laborCost + materialCost){ alert("The cost equals"); } 
+ 
+//  if (true){ alert("True"); } if (false){ alert("False"); }
+
+// T=+prompt("Enter today tempreture");
+// if(T>40){
+//     alert("it is to hot outside")
+// }
+// else if(T>30){
+//     alert("Today Weather is Normal")
+// }
+// else if(T>20 ){
+//     alert("Today's weather is cool")
+// }
+// else if(T>10){
+//     alert("OMG today weather is so cold")
+// }
+
+// var first_number=+prompt("Enter first Number");
+// var exprssion=+prompt("Enter your expression","+,-,*,/")
+// var second_number=+prompt("Enter your second number");
+
+// if("exprssion== + " ){
+//     alert(first_number+second_number)
+// }
+
+// else if("exprssion==-"){
+//     alert(first_number - second_number)
+// }
+// else if("exprssion===-"){
+//     alert(first_number - second_number)
+// }
+
+// else if("exprssion===*"){
+//     alert(first_number * second_number)
+// }
+// else if("exprssion===/"){
+//     alert(first_number / second_number)
+// }
+// else{
+//     alert("Wrong Entry")
+// }
+
+// chap # 12-13
+
+// var num1, num2;
+// num1 = +prompt("Input the First integer", "0");
+// num2 = +prompt("Input the second integer", "0");
+                                                 
+// if((num1, 10) > (num2, 10)) 
+//   { 
+//  alert("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+//   }   
+// else
+//   if((num2, 10) > (num1, 10)) 
+//   {
+// alert("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+//   }                  
+// else
+//   {
+//    alert("The values "+ num1+ " and "+num2+ " are equal.");
+//   }
+
+// var a=+prompt("Enter your number is","0")
+// if (a<0){
+//     alert("Nagitive number")
+// }
+// else if (a>0){
+//     alert("Positive number")
+// }
+// else if(a=0){
+//     alert("0")
+// }
+
+// var a =+prompt("Enter your value");
+// if(a.length >=3){
+//     alert("True")
+// }
+// else{
+//     alert("False")
+// }        
+
+
+// var password=+prompt("Enter your password");
+// if(password==12345){
+//     alert("Correct")
+
+// }
+// else if(password!=12345){
+//     alert("wrong Password")
+// }
+// else if(password===null){
+//     alert("Pasword must be required")
+// }
+
+// var greeting; 
+// var hour = 13;
+//  if (hour < 18) { greeting = "Good day";}
+//   else { greeting ="Good evening"; } 
+ 
+//  alert (greeting)
+
+// var time=1900;
+// if (time>=00 && time<1200){
+//     alert("Good Morning")
+// }
+// else if(time>=1200 && time<1700){
+//     alert("Good Afternon")
+// }
+
+// else if(time>=1700 && time<2100){
+//     alert("Good Evening")
+// }
+
+// else if(time>=2100 && time<=2359){
+//     alert("Good Night")
+// }
+
+
+// Chap #14 to 16
+// var student_name=[];
